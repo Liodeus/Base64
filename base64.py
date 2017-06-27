4dac0ba4139a2ec5d6ece3e7d34f9bbc8e619fc6
Liodeus <thibaultgalbourdin@live.fr>
2017-06-27 15:04:51 +0200

    Base64 encoder is working, now it add the = characters if needed

--- base64.py
+++ base64.py
@@ -1,8 +1,9 @@
 #!/bin/usr/env Python3
 # -*- encoding:utf_8 -*-
 
+import Base64Table
 
-t = "Hi!"
+t = "sure."
 liste = []
 for letter in t:
     test = bin(ord(letter))[2:]
@@ -15,4 +16,26 @@ for letter in t:
 
     liste.append(test)
 
-print("".join(liste))
+
+liste2 = []
+fullString = "".join(liste)
+
+# Found -> https://gist.github.com/bellbind/255287
+te = (fullString[i:i + 6] for i in range(0, len(fullString), 6))
+
+l = ""
+s2 = ""
+for t in te:
+    if len(t) != 6:
+        size = 6 - len(t)
+        for o in range(0, size):
+            t += '0'
+    s2 += t
+    l += Base64Table.dictio[t]
+
+if (len(s2) + 6) % 24 == 0:
+    l += "="
+elif (len(s2) + 12) % 24 == 0:
+    l += "=="
+
+print(l)
